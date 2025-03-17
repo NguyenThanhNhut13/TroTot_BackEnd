afa035eec57da5d2de236b105216b6c910c8ac42
vantoan24-iuh <vantoan24-iuh>
2025-03-17 16:28:23 +0700

    update reportAPI

---
+++ report-service/src/main/java/vn/edu/iuh/fit/reportservice/entity/Report.java
@@ -0,0 +1,102 @@
+package vn.edu.iuh.fit.reportservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "reports")
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long roomId;
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private ReportType type;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private ReportStatus status;
+
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+
+    public void resolve() {
+        this.status = ReportStatus.RESOLVED;
+        this.updateAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public ReportType getType() {
+        return type;
+    }
+
+    public void setType(ReportType type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ReportStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ReportStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+}
---
+++ report-service/src/main/java/vn/edu/iuh/fit/reportservice/entity/ReportType.java
@@ -0,0 +1,6 @@
+package vn.edu.iuh.fit.reportservice.entity;
+
+public enum ReportType {
+    SPAM, INAPPROPRIATE, FAKE, SCAM;
+
+}
