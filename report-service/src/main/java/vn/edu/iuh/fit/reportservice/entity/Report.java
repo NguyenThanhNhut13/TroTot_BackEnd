1ead6af9ed1da438e7e72899a925e7e94a46f336
vantoan24-iuh <vantoan24-iuh>
2025-03-19 15:31:26 +0700

    update

---
+++ report-service/src/main/java/vn/edu/iuh/fit/reportservice/entity/Report.java
@@ -0,0 +1,106 @@
+package vn.edu.iuh.fit.reportservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "reports"
+)
+public class Report {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+    private Long roomId;
+    private Long userId;
+    @Enumerated(EnumType.STRING)
+    private ReportType type;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private ReportStatus status;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+
+    public Report() {
+    }
+
+    public void resolve() {
+        this.status = ReportStatus.RESOLVED;
+        this.updateAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public ReportType getType() {
+        return type;
+    }
+
+    public void setType(ReportType type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ReportStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ReportStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+}
+
