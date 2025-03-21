b3873636edf3a644b35190bdda3ac87e3c33987b
NguyenThanhNhut13 <thanhnhutcu@gmail.com>
2025-03-21 19:51:13 +0700

    update config api-gateway

---
+++ api-gateway/src/main/java/vn/edu/iuh/fit/apigateway/config/GatewayConfig.java
@@ -0,0 +1,16 @@
+/*
+ * @ (#) GatewayConfig.java       1.0     21/03/2025
+ * 
+ * Copyright (c) 2025 IUH. All rights reserved.
+ */
+ 
+package vn.edu.iuh.fit.apigateway.config;
+/*
+ * @description:
+ * @author: Nguyen Thanh Nhut
+ * @date: 21/03/2025
+ * @version:    1.0
+ */
+
+public class GatewayConfig {
+}
---
+++ auth-service/src/main/java/vn/edu/iuh/fit/authservice/exception/InvalidCredentialsException.java
@@ -0,0 +1,16 @@
+/*
+ * @ (#) InvalidCredentialsException.java       1.0     21/03/2025
+ * 
+ * Copyright (c) 2025 IUH. All rights reserved.
+ */
+ 
+package vn.edu.iuh.fit.authservice.exception;
+/*
+ * @description:
+ * @author: Nguyen Thanh Nhut
+ * @date: 21/03/2025
+ * @version:    1.0
+ */
+
+public class InvalidCredentialsException {
+}
