8e1c7f3f9803cad435759c7d46a96b06373a573a
NguyenThanhNhut13 <thanhnhutcu@gmail.com>
2025-04-10 00:03:44 +0700

    add media-service, add api upload single, multi files and delete file

--- api-gateway/src/main/java/vn/edu/iuh/fit/apigateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/vn/edu/iuh/fit/apigateway/filter/AuthenticationFilter.java
@@ -47,6 +47,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Object> {
     private static final Map<String, List<String>> PUBLIC_ENDPOINTS = Map.of(
             "GET", List.of(
                     "/api/v1/payments/vn-pay-callback",
+                    "/api/v1/rooms",
                     "/api/v1/auth/login",
                     "/api/v1/auth/register",
                     "/api/v1/auth/verify-otp",
