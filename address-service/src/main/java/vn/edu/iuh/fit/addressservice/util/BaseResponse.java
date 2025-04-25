d15d882ab95fb4afd4eb8cfbd4ae18de3c4431bb
NguyenTongAnhQuan443 <ntanhquan.sly@gmail.com>
2025-04-25 08:53:33 +0700

    Update description Address Controller

--- address-service/src/main/java/vn/edu/iuh/fit/addressservice/util/BaseResponse.java
+++ address-service/src/main/java/vn/edu/iuh/fit/addressservice/util/BaseResponse.java
@@ -1,12 +1,20 @@
 package vn.edu.iuh.fit.addressservice.util;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "Base response chuẩn cho tất cả API")
 public class BaseResponse<T> {
+    @Schema(description = "Trạng thái thành công hay thất bại", example = "true")
     private boolean success;
+
+    @Schema(description = "Thông báo đi kèm", example = "Success")
     private String message;
+
+    @Schema(description = "Dữ liệu trả về (có thể null hoặc object/list)", nullable = true)
     private T data;
 
     public static <T> BaseResponse<T> ok(T data) {
