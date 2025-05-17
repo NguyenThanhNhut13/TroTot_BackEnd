a961dde5fb632671aa58da88a9097bc0ddb1deac
NguyenThanhNhut13 <thanhnhutcu@gmail.com>
2025-05-17 17:51:17 +0700

    add file entrypoint.sh

---
+++ config-server/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Starting Config Server..."
+
+# Optional: liệt kê secret để debug
+echo "Contents of /etc/secrets:"
+ls /etc/secrets
+
+# Đặt biến môi trường để Spring Boot đọc từ file config & cert
+export SPRING_CONFIG_ADDITIONAL_LOCATION=file:/etc/secrets/configurations/
+
+# Khởi động Spring Boot app
+exec java -jar config-server.jar
