be81dabe15aa6110d4fab2fc51eefd7ba0ae1efc
NguyenThanhNhut13 <thanhnhutcu@gmail.com>
2025-05-17 19:43:01 +0700

    update entrypoint.sh

--- config-server/entrypoint.sh
+++ config-server/entrypoint.sh
@@ -7,7 +7,7 @@ echo "Contents of /etc/secrets:"
 ls /etc/secrets
 
 # Đặt biến môi trường để Spring Boot đọc từ file config & cert
-export SPRING_CONFIG_ADDITIONAL_LOCATION=file:/etc/secrets/configurations/
+export SPRING_CONFIG_ADDITIONAL_LOCATION=file:/etc/secrets/
 
 # Khởi động Spring Boot app
 exec java -jar config-server.jar
