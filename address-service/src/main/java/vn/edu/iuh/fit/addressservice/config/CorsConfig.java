f930b642e54c5b6bce291b3848b8665b39c2badd
NguyenThanhNhut13 <thanhnhutcu@gmail.com>
2025-05-22 21:35:01 +0700

    update cors

--- address-service/src/main/java/vn/edu/iuh/fit/addressservice/config/CorsConfig.java
+++ address-service/src/main/java/vn/edu/iuh/fit/addressservice/config/CorsConfig.java
@@ -15,9 +15,11 @@ package vn.edu.iuh.fit.addressservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
